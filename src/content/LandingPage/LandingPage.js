705db15934133cd17c0f2155e8c73ee6ef88275a
natagr23 <nata_garcia23@hotmail.com>
2022-09-05 14:23:53 -0500

    changing background

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -92,36 +92,159 @@ const LandingPage = () => {
             <TabPanel>
               <Grid className="tabs-group-content">
                 <Column
-                  lg={16}
-                  md={8}
-                  sm={4}
-                  className="landing-page__tab-content"
-                ></Column>
-              </Grid>
-            </TabPanel>
-            <TabPanel>
-              <Grid className="tabs-group-content">
-                <Column
-                  lg={16}
-                  md={8}
+                  md={4}
+                  lg={7}
                   sm={4}
                   className="landing-page__tab-content"
                 >
-                  Carbon provides styles and components in Vanilla, React,
-                  Angular, and Vue for anyone building on the web.
+                  <h2 className="landing-page__subheading">Experience</h2>
+                  <p className="landing-page__p2">
+                    I have knowledge in web development (React.js, Javascript,
+                    HTML5, CSS, Carbon Design, MUI,Bootstrap, Tailwind,
+                    Firebase). During my master's studies I used meteorological
+                    data to evaluate a hydrological model using R.
+                  </p>
+                  <hr />
+                  <Link
+                    href={'https://www.linkedin.com/in/nataliagarciarosas/'}
+                  >
+                    <Button>
+                      {' '}
+                      <LogoLinkedin size={20} />
+                    </Button>
+                  </Link>
+                  <hr />
+                  <Link href={'https://github.com/natagr23'}>
+                    <Button>
+                      <LogoGithub size={20} />
+                      {/* Github Profile */}
+                    </Button>
+                  </Link>
+                  <hr />
+                  <Link
+                    href={
+                      'mailto:nata_garcia23@hotmail.com?body=My custom mail body'
+                    }
+                  >
+                    <Button>
+                      <Email size={20} />
+                      Contact me
+                    </Button>
+                  </Link>
+                  <hr />
+                </Column>
+                <Column md={4} lg={{ span: 8, offset: 7 }} sm={4}>
+                  <img
+                    className="landing-page__illo"
+                    src={profileImg}
+                    alt="Carbon illustration"
+                  />
                 </Column>
               </Grid>
             </TabPanel>
             <TabPanel>
               <Grid className="tabs-group-content">
                 <Column
-                  lg={16}
-                  md={8}
+                  md={4}
+                  lg={7}
                   sm={4}
                   className="landing-page__tab-content"
                 >
-                  Carbon2222 provides styles and components in Vanilla, React,
-                  Angular, and Vue for anyone building on the web.
+                  <h2 className="landing-page__subheading">Experience</h2>
+                  <p className="landing-page__p2">
+                    I have knowledge in web development (React.js, Javascript,
+                    HTML5, CSS, Carbon Design, MUI,Bootstrap, Tailwind,
+                    Firebase). During my master's studies I used meteorological
+                    data to evaluate a hydrological model using R.
+                  </p>
+                  <hr />
+                  <Link
+                    href={'https://www.linkedin.com/in/nataliagarciarosas/'}
+                  >
+                    <Button>
+                      {' '}
+                      <LogoLinkedin size={20} />
+                    </Button>
+                  </Link>
+                  <hr />
+                  <Link href={'https://github.com/natagr23'}>
+                    <Button>
+                      <LogoGithub size={20} />
+                      {/* Github Profile */}
+                    </Button>
+                  </Link>
+                  <hr />
+                  <Link
+                    href={
+                      'mailto:nata_garcia23@hotmail.com?body=My custom mail body'
+                    }
+                  >
+                    <Button>
+                      <Email size={20} />
+                      Contact me
+                    </Button>
+                  </Link>
+                  <hr />
+                </Column>
+                <Column md={4} lg={{ span: 8, offset: 7 }} sm={4}>
+                  <img
+                    className="landing-page__illo"
+                    src={profileImg}
+                    alt="Carbon illustration"
+                  />
+                </Column>
+              </Grid>
+            </TabPanel>
+            <TabPanel>
+              <Grid className="tabs-group-content">
+                <Column
+                  md={4}
+                  lg={7}
+                  sm={4}
+                  className="landing-page__tab-content"
+                >
+                  <h2 className="landing-page__subheading">Experience</h2>
+                  <p className="landing-page__p2">
+                    I have knowledge in web development (React.js, Javascript,
+                    HTML5, CSS, Carbon Design, MUI,Bootstrap, Tailwind,
+                    Firebase). During my master's studies I used meteorological
+                    data to evaluate a hydrological model using R.
+                  </p>
+                  <hr />
+                  <Link
+                    href={'https://www.linkedin.com/in/nataliagarciarosas/'}
+                  >
+                    <Button>
+                      {' '}
+                      <LogoLinkedin size={20} />
+                    </Button>
+                  </Link>
+                  <hr />
+                  <Link href={'https://github.com/natagr23'}>
+                    <Button>
+                      <LogoGithub size={20} />
+                      {/* Github Profile */}
+                    </Button>
+                  </Link>
+                  <hr />
+                  <Link
+                    href={
+                      'mailto:nata_garcia23@hotmail.com?body=My custom mail body'
+                    }
+                  >
+                    <Button>
+                      <Email size={20} />
+                      Contact me
+                    </Button>
+                  </Link>
+                  <hr />
+                </Column>
+                <Column md={4} lg={{ span: 8, offset: 7 }} sm={4}>
+                  <img
+                    className="landing-page__illo"
+                    src={profileImg}
+                    alt="Carbon illustration"
+                  />
                 </Column>
               </Grid>
             </TabPanel>
