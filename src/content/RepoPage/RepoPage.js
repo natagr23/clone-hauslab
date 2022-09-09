f0f1d2a95ccd88f4426ecfb65dd253c389873044
natagr23 <nata_garcia23@hotmail.com>
2022-09-08 19:04:59 -0500

    adding button with file save option

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import pdf from '../../Data/CV_Natalia_Garcia_ENG.pdf';
+import { saveAs } from 'file-saver';
 
 const RepoPage = () => {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
+
+  const saveFile = () => {
+    saveAs(pdf, 'CV_NGR.pdf');
+  };
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
@@ -12,12 +17,13 @@ const RepoPage = () => {
 
   return (
     <div>
+      <button onClick={saveFile}>download</button>
       <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
         <Page pageNumber={pageNumber} />
       </Document>
-      <p>
+      {/* <p>
         Page {pageNumber} of {numPages}
-      </p>
+      </p> */}
     </div>
   );
 };
